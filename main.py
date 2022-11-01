f455aa56bd473e7d30f3d085944e44cf48e85951
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2022-11-01 00:35:34 -0600

    optimizations

--- main.py
+++ main.py
@@ -2,47 +2,52 @@
 import pygame
 from OpenGL.GL import *
 
-def paint_grid(screen):
-  for y in range(H):
-    for x in range(W):
-      last = GRID[y][x][actualgrid]
-      actual = GRID[y][x][nextgrid]
+def pixel(x, y, color):
+  glEnable(GL_SCISSOR_TEST)
+  glScissor(x, y, blocksize-1, blocksize-1)
+  glClearColor(*color, 1)
+  glClear(GL_COLOR_BUFFER_BIT)
+  glDisable(GL_SCISSOR_TEST)
 
-      if last != actual:
-        color = (255, 255, 215) if last else (10, 10, 40)
-        pygame.draw.rect(screen, color, pygame.Rect(x*blocksize, y*blocksize, blocksize-1, blocksize-1))
+def draw_rect(x, y, color):
+  pygame.draw.rect(
+    screen,
+    color,
+    (x*blocksize, H - y*blocksize, blocksize-1, blocksize-1)
+  )
 
-def init_grid(screen):
+def paint_grid():
+  while len(changes) > 0:
+    x, y = changes.pop()
+    color = (255, 255, 215) if GRID[y][x][actualgrid] else (10, 10, 40)
+    draw_rect(x, y, color)
+    
+def init_grid(pattern = []):
+  for y in range(len(pattern)):
+    for x in range(len(pattern[0])):
+      if pattern[y][x] == 1:
+        set_grid(x, y)
+
   for y in range(H):
     for x in range(W):
       color = (255, 255, 215) if GRID[y][x][0] else (10, 10, 40)
-      pygame.draw.rect(screen, color, pygame.Rect(x*blocksize, y*blocksize, blocksize-1, blocksize-1))
-
+      draw_rect(x, y, color)
+      
 def __getVecinos(x, y, GRID, actualgrid):
-  xi, yi = x+1, y+1
-
-  try:
-    u1 = GRID[y][x+1][actualgrid]
-  except:
-    xi = 0
+  xi = x+1 if x+1 < len(GRID[0]) else 0
+  yi = y+1 if y+1 < len(GRID) else 0
   
-  try:
-    u2 = GRID[y+1][x][actualgrid]
-  except:
-    yi = 0
-  
-  vecinos:list = [
-    GRID[y][xi][actualgrid],
-    GRID[yi][x][actualgrid],
-    GRID[yi][xi][actualgrid],
-    GRID[yi][x-1][actualgrid],
-    GRID[y-1][x-1][actualgrid],
-    GRID[y-1][xi][actualgrid],
-    GRID[y-1][x][actualgrid],
-    GRID[y][x-1][actualgrid]
-  ]
+  vecinos_count = 0
+  vecinos_count += 1 if GRID[y][xi][actualgrid] else 0
+  vecinos_count += 1 if GRID[yi][x][actualgrid] else 0
+  vecinos_count += 1 if GRID[yi][xi][actualgrid] else 0
+  vecinos_count += 1 if GRID[yi][x-1][actualgrid] else 0
+  vecinos_count += 1 if GRID[y-1][x-1][actualgrid] else 0
+  vecinos_count += 1 if GRID[y-1][xi][actualgrid] else 0
+  vecinos_count += 1 if GRID[y-1][x][actualgrid] else 0
+  vecinos_count += 1 if GRID[y][x-1][actualgrid] else 0
 
-  return vecinos.count(True)
+  return vecinos_count
 
 def compute_game(actualgrid, nextgrid):
   for y in range(H):
@@ -53,9 +58,11 @@ def compute_game(actualgrid, nextgrid):
 
       if cell and alive not in [2, 3]:
         GRID[y][x][nextgrid] = False
+        changes.append((x, y))
       
       if not cell and alive == 3:
         GRID[y][x][nextgrid] = True
+        changes.append((x, y))
 
   return nextgrid, actualgrid
 
@@ -63,9 +70,8 @@ def set_grid(x, y, value=True):
   GRID[y][x][0] = value
 
 if __name__ == '__main__':
-  blocksize = 20
-  windowsize = (800, 600)
-  W, H = windowsize
+  blocksize = 15
+  W, H = (800, 600)
 
   GRID = [
     [[False, False] for x in range(W)]
@@ -73,33 +79,31 @@ if __name__ == '__main__':
   ]
 
   pattern = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
-    [1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],
+    [0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   ]
-  for y in range(len(pattern)):
-    for x in range(len(pattern[0])):
-      if pattern[y][x] == 1:
-        set_grid(x, y)
-
+  
   pygame.display.set_caption("John Conway's Game of Life")
-  screen = pygame.display.set_mode(windowsize, pygame.DOUBLEBUF)
-  init_grid(screen)
+  screen = pygame.display.set_mode((W, H))
+  init_grid(pattern)
   pygame.init()
 
-  actualgrid = 0
-  nextgrid = 1
+  actualgrid, nextgrid = 0, 1
   running = True
+  changes = []
 
   while running:
     # Paint_grid
-    paint_grid(screen)
+    paint_grid()
 
     # Operate iteration
     actualgrid, nextgrid = compute_game(actualgrid, nextgrid)
@@ -108,5 +112,7 @@ if __name__ == '__main__':
     pygame.display.update()
 
     for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        running = False
+      match event.type:
+        case pygame.QUIT:
+          running = False
+  
---
+++ run.sh
@@ -0,0 +1,2 @@
+pip3 ./main.py
+clear
